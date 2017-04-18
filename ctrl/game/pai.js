445522d79f359334b7dbadbf71966703de6f0a2d
AlexYu1987 <alexyu87@outlook.com>
2017-04-18 15:47:55 +0800

    Complete pai

--- ctrl/game/pai.js
+++ ctrl/game/pai.js
@@ -140,8 +140,25 @@ function printCard(card) {
 	return color + num;
 }
 
+//获取牌面分值
 function getFen(c) {
-	if (card < 52)
+	if (c < 52) return c % 13 + 1;
+	else return c + 1;
+}
+
+//A映射成14,2映射成15,保证入参小于52
+function map3To15(c) {
+	c = c % 13 + 1;
+	if (c == 1) return 14;
+	if (c == 2) return 15;
+	return c;
+}
+
+//A映射成14
+function map2To14(c) {
+	c = c % 13 + 1;
+	if (c == 1) return 14;
+	return c;
 }
 
 function isShunZi(vCard) {
@@ -150,9 +167,9 @@ function isShunZi(vCard) {
 	vCard.sort(sortble);
 	var vCardTmp = [];
 	var jokeFlag = false
-	//将牌号处理成分值，过滤掉大小王（不能作为顺子），并处理A作为13
+		//将牌号处理成分值，过滤掉大小王（不能作为顺子），并处理A作为13
 	vCard.forEach(function(c) {
-		if (c >= 52){
+		if (c >= 52) {
 			jokeFlag = true;
 			return;
 		} else {
@@ -177,13 +194,13 @@ function compareShunZi(vCard1, vCard2) {
 
 	var vTmp1 = [];
 	var vTmp2 = [];
-	vCard1.forEach(function(c){
+	vCard1.forEach(function(c) {
 		c %= 13;
 		if (c == 0) c = 13;
 		vTmp1.push(c);
 	});
 
-	vCard2.forEach(function(c){
+	vCard2.forEach(function(c) {
 		c %= 13;
 		if (c == 0) c = 13;
 		vTmp1.push(c);
@@ -192,5 +209,202 @@ function compareShunZi(vCard1, vCard2) {
 	vTmp1.sort(sortble);
 	vTmp2.sort(sortble);
 
-	return vTmp1[0] - vTmp2[0];
+	return vTmp1[0] - vTmp2[0] > 0 ? 1 : -1;
+}
+
+function isFeiJi(vCard) {
+	if (vCard.length != 6) return false;
+	vCard.sort(sortble);
+
+	var jokeFlag = false;
+	var tmpCard = [];
+	vCard.forEach(function(c) {
+		if (c >= 52) {
+			jokeFlag = true;
+			return;
+		}
+		tmpCard.push(map2To14(c));
+	});
+
+	if (jokeFlag) return false;
+
+	if ((tmpCard[0] == tmpCard[1] == tmpCard[2]) && (tmpCard[3] == tmpCard[4] == tmpCard[5]) && (tmpCard[2] + 1 == tmpCard[3])) return true;
+
+	return false;
+}
+
+//调用前需要保证前后参数都是飞机
+function compareFeiJi(vCard1, vCard2) {
+	vCard1.sort(sortble);
+	vCard2.sort(sortble);
+
+	return map2To14(vCard1[0]) - map2To14(vCard2[0]) > 0 ? 1 : -1;
+}
+
+function is4P2(vCard) {
+	if (vCard.length != 6) return false;
+	vcard.sort(sortble);
+
+	if ((map3To15(vCard[0]) == map3To15(vCard[1]) == map3To15(vCard[2]) == map3To15(vCard[3])) && (map3To15(vCard[4]) == map3To15(vCard[5]))) {
+		return true;
+	}
+
+	if ((map3To15(vCard[2]) == map3To15(vCard[3]) == map3To15(vCard[4]) == map3To15(vCard[5])) && (map3To15(vCard[0]) == map3To15(vCard[1]))) {
+		return true;
+	}
+
+	return false;
+}
+
+function compare4P2(vCard1, vCard2) {
+	vCard1.sort(sortble);
+	vCard2.sort(sortble);
+
+	var c1 = -1,
+		c2 = -1;
+	if ((map2To14(vCard1[0]) == map2To14(vCard1[1]) == map2To14(vCard1[2]) == map2To14(vCard1[3]))) {
+		c1 = vCard1[0];
+	} else {
+		c1 = vCard1[5];
+	}
+
+	if ((map2To14(vCard2[0]) == map2To14(vCard2[1]) == map2To14(vCard2[2]) == map2To14(vCard2[3]))) {
+		c2 = vCard2[0];
+	} else {
+		c2 = vCard2[5];
+	}
+
+	return map3To15(vCard1[0]) - map3To15(vCard2[0]) > 0 ? 1 : -1;
+}
+
+function isLianDui(vCard) {
+	if (vCard.length < 6 || vCard.length % 2 != 0) return false;
+
+	vCard.sort(sortble);
+	for (var i = 0; i < vCard.length; i += 2) {
+		if (i == 0) {
+			if (map2To14(vCard[i]) == map2To14(vCard[i + 1])) continue;
+			else return false;
+		} else {
+			if (map2To14(vCard[i]) == map2To14(vCard[i + 1]) && map2To14(vCard[i - 1]) - 1 == map2To14(vCard[i])) continue;
+			else return false
+		}
+	}
+	return true;
+}
+
+function compareLianDui(vCard1, vCard2) {
+	vCard1.sort(sortble);
+	vCard2.sort(sortble);
+	return map2To14(vCard1[0]) - map2To14(vCard2[0]) > 0 ? 1 : -1;
+}
+
+function isBoomb(vCard) {
+	if (vCard.length != 4) return false;
+	if (map2To14(vCard[0]) == map2To14(vCard[1]) == map2To14(vCard[2]) == map2To14(vCard[3])) return true;
+	return false;
+}
+
+function compareBoomb(vCard1, vCard2) {
+	return map2To14(vCard1[0]) - map2To14(vCard2[0]) > 0 ? 1 : -1;
+}
+
+function is3P2(vCard) {
+	if (vCard.length != 5) return false;
+	vCard.sort(sortble);
+	if (map3To15(vCard[0]) == map3To15(vCard[1]) == map3To15(vCard[2]) && map3To15(vCard[3]) == map3To15(vCard[4])) return true;
+	if (map3To15(vCard[2]) == map3To15(vCard[3]) == map3To15(vCard[4]) && map3To15(vCard[0]) == map3To15(vCard[1])) return true;
+	return false;
+}
+
+function compare3P2(vCard1, vCard2) {
+	vCard1.sort(sortble);
+	vCard2.sort(sortble);
+
+	var c1 = -1;
+	var c2 = -1;
+	if (map3To15(vCard1[0]) == map3To15(vCard1[1]) == map3To15(vCard1[2])) {
+		c1 = map3To15(vCard1[0]);
+	} else {
+		c1 = map3To15(vCard1[4]);
+	}
+
+	if (map3To15(vCard2[0]) == map3To15(vCard2[1]) == map3To15(vCard2[2])) {
+		c2 = map3To15(vCard2[0]);
+	} else {
+		c2 = map3To15(vCard2[4]);
+	}
+
+	return c1 - c2 > 0 ? 1 : -1;
+}
+
+function is3P1(vCard) {
+	if (vCard.length != 4) return false;
+	vCard.sort(sortble);
+	if (map3To15(vCard[0]) == map3To15(vCard[1]) == map3To15(vCard[2])) return true;
+	if (map3To15(vCard[1]) == map3To15(vCard[2]) == map3To15(vCard[3])) return true;
+	return false;
+}
+
+function compare3P1(vCard1, vCard2) {
+	vCard1.sort(sortble);
+	vCard2.sort(sortble);
+
+	var c1 = -1;
+	var c2 = -1;
+	if (map3To15(vCard1[0]) == map3To15(vCard1[1]) == map3To15(vCard1[2])) {
+		c1 = map3To15(vCard1[0]);
+	} else {
+		c1 = map3To15(vCard1[3]);
+	}
+
+	if (map3To15(vCard2[0]) == map3To15(vCard2[1]) == map3To15(vCard2[2])) {
+		c2 = map3To15(vCard2[0]);
+	} else {
+		c2 = map3To15(vCard2[3]);
+	}
+
+	return c1 - c2 > 0 ? 1 : -1;
+}
+
+function is3P0(vCard) {
+	if (vCard.length != 3) return false;
+	if (map3To15(vCard[0]) == map3To15(vCard[1]) == map3To15(vCard[2])) return true;
+	else return false;
+}
+
+function compare3P0(vCard1, vCard2) {
+	vCard1.sort(sortble);
+	vCard2.sort(sortble);
+
+	var c1 = map3To15(vCard1[0]);
+	var c2 = map3To15(vCard2[0]);
+
+	return c1 - c2 > 0 ? 1 : -1;
+}
+
+function isDouble(vCard) {
+	if (vCard.length != 2) return false;
+	if (map3To15(vCard[0]) != map3To15(vCard[1])) return false;
+
+	return true;
+}
+
+function compareDouble(vCard1, vCard2) {
+	return map3To15(vCard1[0]) - map3To15(vCard2[0]) > 0 ? 1 : -1;
+}
+
+function isTianWang(vCard) {
+	if (vCard.length != 2) return false;
+
+	var f = true;
+	vCard.forEach(function(c){
+		if(c != 52 || c != 53) f = false;
+	});
+
+	return f;
+}
+
+function compareSingle(vCard1, vCard2) {
+	return map3To15(vCard1[0]) - map3To15(vCard2[0]) > 0 ? 1 : -1;
 }
